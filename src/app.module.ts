b99d688735834e4bdc97d013ea20d8498789defe
Anuolu-2020 <Anuolu1645@gmail.com>
2024-03-31 23:06:22 +0100

    chore: Add Prisma and Auth modules to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 import * as Joi from 'joi';
 
 @Module({
@@ -19,6 +21,8 @@ import * as Joi from 'joi';
         abortEarly: false,
       },
     }),
+    PrismaModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
