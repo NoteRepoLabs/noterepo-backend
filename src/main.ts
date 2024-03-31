15750d381f3737dcb6944d24c2802ca7d4d36a34
Anuolu-2020 <Anuolu1645@gmail.com>
2024-03-31 23:06:37 +0100

    feat: Add API prefix and enable URI versioning

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,22 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({ logger: true }),
   );
+
+  //add api prefix to all endpoints
+  app.setGlobalPrefix('api');
+
+  //For versioning api
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
