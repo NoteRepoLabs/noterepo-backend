03869aa21017adb75351a82b530c09f7df1c0d43
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-01 20:54:19 +0100

    chore: import jwt service and cookie service to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtService } from 'src/jwt/jwt.service';
+import { CookieService } from 'src/cookie/cookie.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService, CookieService],
 })
 export class AuthModule {}
