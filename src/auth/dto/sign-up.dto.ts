bd00396a5b94c68fc2be3a3329764c4b09042723
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-01 20:56:40 +0100

    feat: implement sign up request body dto

--- src/auth/dto/sign-up.dto.ts
+++ src/auth/dto/sign-up.dto.ts
@@ -1,10 +1,12 @@
 import * as Joi from 'joi';
 
+// Request Body dto for sign-up route
 export class SignUpDto {
     email: string
     password: string
 }
 
+// Request body schema for sign-up route
 export const signUpSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).max(15).required(),
