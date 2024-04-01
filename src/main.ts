2b55d2384fc721e783dad68cce49b68400c7bdff
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-01 20:52:58 +0100

    feat: implement cookie support for fastify and function needed for class transformer to work

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import { VersioningType } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  VersioningType,
+} from '@nestjs/common';
+
+import fastifyCookie from '@fastify/cookie';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -12,7 +18,18 @@ async function bootstrap() {
     new FastifyAdapter({ logger: true }),
   );
 
-  //add api prefix to all endpoints
+  //Needed for response dtos to function
+  registerGlobals(app);
+
+  //Adding cookie support to fatisfy
+  app.register(fastifyCookie, {
+    secret: process.env.COOKIE_SECRET,
+    parseOptions: {
+      path: '/',
+    },
+  });
+
+  //Add api prefix to all endpoints
   app.setGlobalPrefix('api');
 
   //For versioning api
@@ -20,6 +37,10 @@ async function bootstrap() {
     type: VersioningType.URI,
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
+
+export function registerGlobals(app: INestApplication) {
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+}
