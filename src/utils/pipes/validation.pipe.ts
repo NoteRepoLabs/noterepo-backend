6c6e4ea76bd24132e5e970e14e38605ae81037bd
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-10 12:12:06 +0100

    refactor: changed validation pipe directory

---
+++ src/utils/pipes/validation.pipe.ts
@@ -0,0 +1,27 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import * as Joi from 'joi';
+
+@Injectable()
+export class AuthValidationPipe implements PipeTransform {
+  constructor(private readonly schema: Joi.ObjectSchema) { }
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (!value) {
+      throw new BadRequestException('Request body is empty');
+    }
+
+    //Validate against Joi schema
+    const { error } = this.schema.validate(value);
+
+    if (error) {
+      throw new BadRequestException(error.message.replace(/"/g, ''));
+    }
+
+    return value;
+  }
+}
