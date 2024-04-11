90f532fe901258a1875f1aa3bd98655878e75429
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-11 18:44:05 +0100

    feat: forget and reset password controller created

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,16 +2,25 @@ import {
   Controller,
   Body,
   Get,
+  Post,
   Patch,
   Param,
   Delete,
   HttpCode,
+  UsePipes,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { plainToInstance } from 'class-transformer';
 import { AuthResponseDto } from 'src/auth/dto/auth-response.dto';
+import { ForgetPasswordDto } from './dto/forget-password.dto';
+import {
+  ResetPasswordDto,
+  resetPasswordSchema,
+} from './dto/reset-password.dto';
+import { AuthValidationPipe } from '../utils/pipes/validation.pipe';
 
 @ApiTags('Users')
 @Controller({ path: 'users', version: '1' })
@@ -29,6 +38,37 @@ export class UsersController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
+  }
+
+  //Forget Password Controller
+  @Post('forget-password')
+  @ApiResponse({
+    status: 200,
+    description: 'Password reset mail sent',
+  })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  async forgetPassword(@Body() body: ForgetPasswordDto) {
+    const response = await this.usersService.forgetPassword(body);
+
+    return { message: response };
+  }
+
+  //Reset Password Controller
+  @Post('reset-password/:userId')
+  @ApiResponse({
+    status: 200,
+    description: 'Password reset successfully',
+  })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  @UsePipes(new AuthValidationPipe(resetPasswordSchema))
+  async resetPassword(
+    @Param('userId', ParseUUIDPipe) id: string,
+    body: ResetPasswordDto,
+  ) {
+    const response = await this.usersService.resetPassword(id, body);
+
+    //Temporary
+    return response;
   }
 
   @ApiResponse({ status: 204, description: 'Deletes a user 🫠' })
