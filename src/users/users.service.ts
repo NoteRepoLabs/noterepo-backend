b260cfea63419d452465a0e084685765ab20af3a
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-11 18:44:19 +0100

    feat: forget and reset password service created

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ForgetPasswordDto } from './dto/forget-password.dto';
+import { generateResetPasswordLink } from 'src/utils/generateLinks/generateResetPasswordLink';
+import { ResetPasswordDto } from './dto/reset-password.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) { }
-  // findAll() {
-  //   return `This action returns all users`;
-  // }
-  //
-  // findOne(id: number) {
-  //   return `This action returns a #${id} user`;
-  // }
-  //
+
+  //Development only
   async getAllUsers() {
     const user = await this.prisma.user.findMany();
 
@@ -21,6 +23,53 @@ export class UsersService {
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} ${updateUserDto} user`;
+  }
+
+  async forgetPassword({ email }: ForgetPasswordDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: email.toLowerCase(),
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User with email not found.');
+    }
+
+    const link = generateResetPasswordLink(user.id);
+
+    //Temporary
+    return link;
+  }
+
+  async resetPassword(
+    id: string,
+    { password, confirmPassword }: ResetPasswordDto,
+  ) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found.');
+    }
+
+    if (password !== confirmPassword) {
+      throw new BadRequestException(
+        'Password does match with confirm password',
+      );
+    }
+
+    const newPassword = await bcrypt.hash(password, 10);
+
+    const updatedUser = this.prisma.user.update({
+      where: { id },
+      data: { password: newPassword },
+    });
+
+    return updatedUser;
   }
 
   //Removes a user
