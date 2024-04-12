e70be2d1aa53ff86b9ebb78f3629e93500a83ce1
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-12 12:49:00 +0100

    docs: improved docs

--- src/users/dto/forget-password.dto.ts
+++ src/users/dto/forget-password.dto.ts
@@ -5,6 +5,8 @@ import { JoiSchema } from 'nestjs-joi';
 export class ForgetPasswordDto {
   @ApiProperty({
     example: 'example@gmail.com',
+    description: 'The email address of the user requesting password reset.',
+    format: 'email',
     required: true,
   })
   @JoiSchema(Joi.string().email().required())
--- src/users/dto/reset-password.dto.ts
+++ src/users/dto/reset-password.dto.ts
@@ -1,11 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import * as Joi from 'joi';
+import { JoiSchema } from 'nestjs-joi';
+import { joiMessages } from 'src/utils/joi/joi.messages';
 
 export class ResetPasswordDto {
+  @ApiProperty({
+    example: 'anony1234',
+    description: 'The new password for the user account',
+    minLength: 6,
+    maxLength: 15,
+    required: true,
+  })
+  @JoiSchema(Joi.string().min(6).max(15).required().messages(joiMessages))
   password: string;
+
+  @ApiProperty({
+    example: 'anony1234',
+    description: 'Confirmation of the new password.',
+    required: true,
+  })
+  @JoiSchema(Joi.string().min(6).max(15).required().messages(joiMessages))
   confirmPassword: string;
 }
-
-export const resetPasswordSchema = Joi.object({
-  password: Joi.string().min(6).max(15).required(),
-  confirmPassword: Joi.string().min(6).max(15).required(),
-});
