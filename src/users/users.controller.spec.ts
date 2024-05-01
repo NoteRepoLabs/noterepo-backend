0745957105c2aab93e2126a611d13b8b467458aa
Anuolu-2020 <Anuolu1645@gmail.com>
2024-05-01 15:17:07 +0100

    fix: fixed minor issues with files paths for test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { EmailService } from '../email/email.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -8,7 +10,7 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [UsersService, PrismaService, EmailService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { EmailModule } from '../email/email.module';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [PrismaModule, EmailModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, PrismaService],
 })
 export class UsersModule { }
