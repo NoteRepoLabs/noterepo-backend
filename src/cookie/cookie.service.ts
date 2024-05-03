bed54ce43d4b997af2459048804eefa6d95c9c6b
Anuolu-2020 <Anuolu1645@gmail.com>
2024-05-03 01:04:07 +0100

    chore: changed cookie domain

--- src/cookie/cookie.service.ts
+++ src/cookie/cookie.service.ts
@@ -7,7 +7,7 @@ export class CookieService {
     return res.cookie('noterepo.auth.token', value, {
       httpOnly: true,
       sameSite: true,
-      domain: 'noterepo.com.ng',
+      domain: 'https://www.noterepo.com.ng',
       secure: process.env.NODE_ENV === 'development' ? false : true,
       expires: new Date(Date.now() + 24 * 60 * 60 * 1000), //1 day
     });
