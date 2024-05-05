8a0bceb626eabc6be674bc47a16a767832798298
Anuolu-2020 <Anuolu1645@gmail.com>
2024-05-05 23:52:43 +0100

    chore: minor change

--- src/cookie/cookie.service.ts
+++ src/cookie/cookie.service.ts
@@ -4,15 +4,10 @@ import { FastifyReply } from 'fastify';
 @Injectable()
 export class CookieService {
   sendCookie(value: string, res: FastifyReply) {
-    const isDevMode = process.env.NODE_ENV === 'development';
-
     return res.cookie('authtoken', value, {
       secure: true,
-      httpOnly: isDevMode ? false : true,
+      httpOnly: true,
       sameSite: 'none',
-      domain: isDevMode
-        ? 'http://localhost:3456'
-        : 'https://www.noterepo.com.ng',
       path: '/',
       expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
     });
