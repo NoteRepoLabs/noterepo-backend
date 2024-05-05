e6c2809ed927902fd6401b9478d0d0d46c9025cc
dev-xero <elo48858@gmail.com>
2024-05-05 21:46:14 +0100

    patch(cookie): Add domain attribute

--- src/cookie/cookie.service.ts
+++ src/cookie/cookie.service.ts
@@ -11,6 +11,9 @@ export class CookieService {
       secure: true,
       httpOnly: false,
       sameSite: 'none',
+      domain: isDevMode
+      ? 'http://localhost:3456'
+      : 'https://www.noterepo.com.ng',
       path: '/',
       expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
     });
