b367d26b4805d236bc7aab789b3268a1718e9578
dev-xero <elo48858@gmail.com>
2024-05-05 21:51:30 +0100

    chore: Tidy up source files

--- src/cookie/cookie.service.ts
+++ src/cookie/cookie.service.ts
@@ -12,8 +12,8 @@ export class CookieService {
       httpOnly: false,
       sameSite: 'none',
       domain: isDevMode
-      ? 'http://localhost:3456'
-      : 'https://www.noterepo.com.ng',
+        ? 'http://localhost:3456'
+        : 'https://www.noterepo.com.ng',
       path: '/',
       expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
     });
