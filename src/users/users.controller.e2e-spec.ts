9aa771da632ae49f2378e5bfd8100b73d7683bc7
Anuolu-2020 <Anuolu1645@gmail.com>
2024-05-11 14:50:40 +0100

    test: implement end to end test for user controller

---
+++ src/users/users.controller.e2e-spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { EmailService } from '../email/email.service';
+import { JwtService } from '../jwt/jwt.service';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [UsersService, PrismaService, EmailService, JwtService],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
