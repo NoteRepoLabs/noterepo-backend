e520f9e6f52cd3364660e4814984968157047c1a
Anuolu-2020 <Anuolu1645@gmail.com>
2024-05-11 14:44:55 +0100

    test: implement test for signup and signin service

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -5,9 +5,13 @@ import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '../jwt/jwt.service';
 import { CookieService } from '../cookie/cookie.service';
 import { EmailService } from '../email/email.service';
+import { prismaService } from '../../test/setupTests.e2e';
+import { FastifyReply } from 'fastify';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let res: FastifyReply;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,12 +23,36 @@ describe('AuthController', () => {
         CookieService,
         EmailService,
       ],
-    }).compile();
+    })
+      .overrideProvider(PrismaService)
+      .useValue(prismaService)
+      .compile();
 
     controller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Should Create User', async () => {
+    const user = { email: 'example123@gmail.com', password: '123456789' };
+
+    const newUser = await controller.signUp(user);
+
+    expect(newUser.email).toBe(user.email);
+    expect(newUser.isVerified).toBe(false);
+    expect(newUser.username).toBeNull();
+    expect(newUser.role).toBe('USER');
+  });
+
+  it('Should not sign in user', async () => {
+    const body = { email: 'example123@gmail.com', password: '123456789' };
+
+    await expect(controller.signIn(body, res)).rejects.toEqual(
+      new UnauthorizedException(
+        'Your account is not verified, an email as be sent to exa*******@gmail.com',
+      ),
+    );
   });
 });
