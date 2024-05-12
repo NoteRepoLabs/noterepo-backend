50c56c175f3381a6f8540c261e588c46998c6297
Anuolu-2020 <Anuolu1645@gmail.com>
2024-05-12 17:33:25 +0100

    chore: cookie and jwt service added to module

--- src/repo/repo.module.ts
+++ src/repo/repo.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { RepoService } from './repo.service';
 import { RepoController } from './repo.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { JwtService } from 'src/jwt/jwt.service';
+import { CookieService } from 'src/cookie/cookie.service';
 
 @Module({
   controllers: [RepoController],
-  providers: [RepoService],
+  providers: [RepoService, JwtService, CookieService],
   imports: [PrismaModule],
 })
 export class RepoModule { }
