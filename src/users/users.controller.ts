3167fdad6242a4bc118a95d279441b545c1d1cd2
Anuolu-2020 <Anuolu1645@gmail.com>
2024-05-12 17:33:49 +0100

    docs: improve api swagger docs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,7 +12,7 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { plainToInstance } from 'class-transformer';
 import { AuthResponseDto } from '../auth/dto/auth-response.dto';
 import { ForgetPasswordDto } from './dto/forget-password.dto';
@@ -24,6 +24,7 @@ import { AuthGuard } from '../guards/auth.guards';
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
+  @ApiOperation({ summary: 'Fetch all users' })
   @ApiResponse({ status: 200, description: 'Fetch all users' })
   @UseGuards(AuthGuard)
   @Get()
@@ -33,6 +34,7 @@ export class UsersController {
     return plainToInstance(AuthResponseDto, users);
   }
 
+  @ApiOperation({ summary: 'Update user by ID' })
   @Patch(':id')
   @UseGuards(AuthGuard)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
@@ -40,6 +42,7 @@ export class UsersController {
   }
 
   //Forget Password Controller
+  @ApiOperation({ summary: 'Send password reset email' })
   @Post('forget-password')
   @ApiResponse({
     status: 200,
@@ -53,6 +56,7 @@ export class UsersController {
   }
 
   //Reset Password Controller
+  @ApiOperation({ summary: 'Reset user password' })
   @Post('reset-password/:userId')
   @ApiResponse({
     status: 200,
@@ -68,6 +72,7 @@ export class UsersController {
     return response;
   }
 
+  @ApiOperation({ summary: 'Delete user by ID' })
   @ApiResponse({ status: 204, description: 'Deletes a user 🫠' })
   @HttpCode(204)
   @UseGuards(AuthGuard)
@@ -76,6 +81,7 @@ export class UsersController {
     return await this.usersService.remove(id);
   }
 
+  @ApiOperation({ summary: 'Delete all users. Not to be used on prod db' })
   @ApiResponse({ status: 204, description: 'All users deleted 💀' })
   @HttpCode(204)
   @Delete()
