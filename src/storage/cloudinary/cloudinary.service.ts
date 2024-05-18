0e17fc31cd928111a1487e9670b3480b27b6ab66
Anuolu-2020 <Anuolu1645@gmail.com>
2024-05-19 00:53:07 +0100

    feat: cloudinary file upload and deletion service implemented

---
+++ src/storage/cloudinary/cloudinary.service.ts
@@ -0,0 +1,47 @@
+import { MultipartFile } from '@fastify/multipart';
+import { Injectable } from '@nestjs/common';
+import { UploadApiResponse, v2 } from 'cloudinary';
+//import toStream = require('buffer-to-stream');
+
+@Injectable()
+export class CloudinaryService {
+  async UploadFile(
+    file: MultipartFile,
+    fileName: string,
+    folderName: string,
+  ): Promise<UploadApiResponse> {
+    //Load file buffer
+    const fileBuffer = await file.toBuffer();
+
+    //Upload file as buffer stream
+    return await new Promise((resolve, reject) => {
+      v2.uploader
+        .upload_stream(
+          {
+            public_id: fileName,
+            resource_type: 'auto',
+            folder: `Users/${folderName}`,
+          },
+          (error, result) => {
+            if (error) {
+              console.log(error);
+              return reject(error);
+            }
+            return resolve(result);
+          },
+        )
+        .end(fileBuffer);
+    });
+  }
+
+  async DeleteFile(fileName: string) {
+    return await v2.uploader.destroy(fileName, (error, result) => {
+      if (error) {
+        console.log(error);
+        return error;
+      }
+
+      return result;
+    });
+  }
+}
