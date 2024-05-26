addfa098dbd88e6b8e4eba5ff9c165c2026ae072
Anuolu-2020 <Anuolu1645@gmail.com>
2024-05-26 18:49:44 +0100

    feat: added temporary rate limiter for file upload

--- src/repo/files/files.controller.ts
+++ src/repo/files/files.controller.ts
@@ -12,13 +12,15 @@ import { FilesService } from './files.service';
 import { FastifyRequest } from 'fastify';
 import { ApiBody, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '../../guards/auth.guards';
+import { Throttle, minutes } from '@nestjs/throttler';
 
 @UseGuards(AuthGuard)
 @ApiTags('Files')
 @Controller({ path: 'users', version: '1' })
 export class FilesController {
   constructor(private readonly filesService: FilesService) { }
-
+  // remeber to change after launch
+  @Throttle({ throttlers: { limit: 15, ttl: minutes(60) } })
   @Post(':userId/repo/:repoId/file')
   @ApiOperation({ summary: 'Uploads a single file to a repo' })
   @ApiConsumes('multipart/form-data')
@@ -54,7 +56,7 @@ export class FilesController {
   }
 
   @Delete(':userId/repo/:repoId/files/:fileId')
-  @ApiOperation({ summary: 'Deletes a multiple files from a repo' })
+  @ApiOperation({ summary: 'Deletes multiple files from a repo' })
   @HttpCode(204)
   async deleteFiles(
     @Param('userId', ParseUUIDPipe) userId: string,
