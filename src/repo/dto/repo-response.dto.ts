8e9789499480a1eb446603ea37a48cbf8389cd01
Anuolu-2020 <Anuolu1645@gmail.com>
2024-06-01 22:14:27 +0100

    chore: change id type to string

--- src/repo/dto/repo-response.dto.ts
+++ src/repo/dto/repo-response.dto.ts
@@ -4,7 +4,7 @@ import { Exclude } from 'class-transformer';
 For excluding some fields in response object eg password
 */
 export class RepoResponseDto {
-  id: number;
+  id: string;
 
   name: string;
 
