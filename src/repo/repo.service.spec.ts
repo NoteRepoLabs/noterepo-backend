0ac34c380520ae20d07f03e34cdf6d230a24afaa
Anuolu-2020 <Anuolu1645@gmail.com>
2024-06-01 22:16:34 +0100

    feat: event emitter module added

--- src/repo/repo.service.spec.ts
+++ src/repo/repo.service.spec.ts
@@ -1,14 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RepoService } from './repo.service';
 import { PrismaService } from '../prisma/prisma.service';
-import {StorageModule} from '../storage/storage.module'
+import { StorageModule } from '../storage/storage.module'
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 describe('RepoService', () => {
   let service: RepoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports:[StorageModule],
+      imports: [StorageModule, EventEmitterModule.forRoot()],
       providers: [RepoService, PrismaService],
     }).compile();
 
