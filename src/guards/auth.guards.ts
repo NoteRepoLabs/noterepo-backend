8231f50ee541f1f04e5275d7d155737796a4efa4
Anuolu-2020 <Anuolu1645@gmail.com>
2024-06-14 16:07:54 +0100

    BREAKING CHANGE!: Authentication is now beare token instead of cookie

--- src/guards/auth.guards.ts
+++ src/guards/auth.guards.ts
@@ -9,30 +9,35 @@ import { JwtService } from '../jwt/jwt.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly jwt: JwtService) { }
+  constructor(private readonly jwtService: JwtService) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     //Extract Cookie from request
-    const token = this.extractTokenFromCookie(request);
-    if (!token) {
-      throw new UnauthorizedException('User Unauthorized');
-    }
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) throw new UnauthorizedException('User Unauthorized');
+
     try {
       //Verify the cookie jwt
-      const payload = await this.jwt.verify(token);
+      const payload = await this.jwtService.verifyAccessToken(token);
 
       request['user'] = payload;
     } catch (err) {
-      throw new UnauthorizedException('Token Expired');
+      throw new UnauthorizedException('Invalid Token or Token Expired');
     }
 
     return true;
   }
 
-  private extractTokenFromCookie(req: FastifyRequest): string | undefined {
-    const token = req.cookies.authtoken;
-    //console.log(`Cookie token ${token}`);
-    return token;
+  private extractTokenFromHeader(req: FastifyRequest): string | undefined {
+    const authHeader = req.headers['authorization'];
+
+    //If no auth header
+    if (!authHeader) return null;
+
+    const [authType, token] = authHeader.split(' ');
+
+    return authType === 'Bearer' ? token : null;
   }
 }
