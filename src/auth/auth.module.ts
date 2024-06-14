559b101d323c3346b16eed59b4bca0368267f64f
Anuolu-2020 <Anuolu1645@gmail.com>
2024-06-14 16:05:26 +0100

    test: added user and cloudinary module with users service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,15 +3,29 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { JwtService } from '../jwt/jwt.service';
-import { CookieService } from '../cookie/cookie.service';
 import { EmailModule } from '../email/email.module';
 import { JoiPipeModule } from 'nestjs-joi';
 import { SearchModule } from '../search/search.module';
 import { SearchService } from '../search/search.service';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from 'src/users/users.service';
+import { CloudinaryService } from 'src/storage/cloudinary/cloudinary.service';
 
 @Module({
-  imports: [PrismaModule, EmailModule, JoiPipeModule, SearchModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    EmailModule,
+    JoiPipeModule,
+    SearchModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService, JwtService, CookieService, SearchService],
+  providers: [
+    AuthService,
+    JwtService,
+    SearchService,
+    UsersService,
+    CloudinaryService,
+  ],
 })
 export class AuthModule { }
