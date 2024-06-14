efe02b48ba27b842715e300a8e023f8c24ca4f36
Anuolu-2020 <Anuolu1645@gmail.com>
2024-06-14 16:08:11 +0100

    feat: refresh token guard implemented

---
+++ src/guards/refreshGuard.guards.ts
@@ -0,0 +1,42 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { FastifyRequest } from 'fastify';
+import { JwtService } from '../jwt/jwt.service';
+
+@Injectable()
+export class RefreshAuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) { }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    //Extract Cookie from request
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) throw new UnauthorizedException('User Unauthorized');
+
+    try {
+      //Verify the cookie jwt
+      const payload = await this.jwtService.verifyRefreshToken(token);
+
+      request['user'] = payload;
+    } catch (err) {
+      throw new UnauthorizedException('Invalid Token or Token Expired');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(req: FastifyRequest): string | undefined {
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader) return null;
+
+    const [authType, token] = authHeader.split(' ');
+
+    return authType === 'Bearer' ? token : null;
+  }
+}
