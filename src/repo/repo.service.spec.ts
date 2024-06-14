b86235b54128b2bfe3af25ebb841c7757fc1ff1a
Anuolu-2020 <Anuolu1645@gmail.com>
2024-06-14 16:10:41 +0100

    test: added new modules

--- src/repo/repo.service.spec.ts
+++ src/repo/repo.service.spec.ts
@@ -1,16 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RepoService } from './repo.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { StorageModule } from '../storage/storage.module'
+import { StorageModule } from '../storage/storage.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
+import { EmailService } from '../email/email.service';
 
 describe('RepoService', () => {
   let service: RepoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [StorageModule, EventEmitterModule.forRoot()],
-      providers: [RepoService, PrismaService],
+      imports: [StorageModule, UsersModule, EventEmitterModule.forRoot()],
+      providers: [RepoService, PrismaService, UsersService, EmailService],
     }).compile();
 
     service = module.get<RepoService>(RepoService);
