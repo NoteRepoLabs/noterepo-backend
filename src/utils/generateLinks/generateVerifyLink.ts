46c10293192383e5b4058d1c55aa1b976b9b50d0
Anuolu-2020 <Anuolu1645@gmail.com>
2024-06-21 16:30:22 +0100

    chore; minor changes

--- src/utils/generateLinks/generateVerifyLink.ts
+++ src/utils/generateLinks/generateVerifyLink.ts
@@ -1,5 +1,5 @@
 export async function generateVerifyLink(verificationId: string) {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV != 'production') {
     return `http://localhost:${process.env.PORT}/api/v1/auth/verifyAccount/${verificationId}`;
   } else {
     return `https://noterepo.onrender.com/api/v1/auth/verifyAccount/${verificationId}`;
