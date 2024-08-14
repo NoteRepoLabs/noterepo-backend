e9a6c3a6e363f2662a1e96cd7fd219221c083b0c
Anuolu-2020 <Anuolu1645@gmail.com>
2024-08-15 00:24:15 +0100

    chore: remove error handler from interceptor

--- src/utils/response/response.interceptor.ts
+++ src/utils/response/response.interceptor.ts
@@ -1,56 +1,29 @@
 import {
-  CallHandler,
-  ExecutionContext,
-  HttpException,
-  HttpStatus,
-  Injectable,
-  NestInterceptor,
-} from '@nestjs/common';
-import { Observable, catchError, map, throwError } from 'rxjs';
+	CallHandler,
+	ExecutionContext,
+	Injectable,
+	NestInterceptor,
+} from "@nestjs/common";
+import { Observable, map } from "rxjs";
 
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
-    return next.handle().pipe(
-      map((res: unknown) => this.responseHandler(res, context)),
-      catchError((err: HttpException) =>
-        throwError(() => this.errorHandler(err, context)),
-      ),
-    );
-  }
+	intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
+		return next
+			.handle()
+			.pipe(map((res: unknown) => this.responseHandler(res, context)));
+	}
 
-  errorHandler(exception: HttpException, context: ExecutionContext) {
-    const ctx = context.switchToHttp();
-    const response = ctx.getResponse();
-    const request = ctx.getRequest();
+	//Response interceptor, still experimenting with this
+	responseHandler(res: any, context: ExecutionContext) {
+		const ctx = context.switchToHttp();
+		const response = ctx.getResponse();
+		//const request = ctx.getRequest();
 
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
-
-    response.status(status).send({
-      status: 'fail',
-      path: request.url,
-      message:
-        status < 500
-          ? exception.message.replace(/"/g, '')
-          : 'An Error Occurred',
-      error_trace:
-        process.env.NODE_ENV === 'development' ? exception.stack : undefined,
-    });
-  }
-
-  //Response interceptor, still experimenting with this
-  responseHandler(res: any, context: ExecutionContext) {
-    const ctx = context.switchToHttp();
-    const response = ctx.getResponse();
-    //const request = ctx.getRequest();
-
-    return {
-      status: 'success',
-      statusCode: response.statusCode,
-      payload: res,
-    };
-  }
+		return {
+			status: "success",
+			statusCode: response.statusCode,
+			payload: res,
+		};
+	}
 }
