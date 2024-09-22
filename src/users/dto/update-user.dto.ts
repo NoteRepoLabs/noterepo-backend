1e93fb9b6e641aa49a7496d37ce14215121cd744
Anuolu-2020 <Anuolu1645@gmail.com>
2024-09-22 18:00:09 +0100

    choe: added validation for updating user bio

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,10 +1,26 @@
-import { Role } from '@prisma/client';
+import { Role } from "@prisma/client";
+import { JoiSchema } from "nestjs-joi";
+import * as Joi from "joi";
+import { ApiProperty } from "@nestjs/swagger";
+import { joiMessages } from "src/utils/joi/joi.messages";
 
 export class UpdateUserDto {
-  username?: string;
-  email?: string;
-  password?: string;
-  isVerified?: boolean;
-  role?: Role;
-  refresh_token?: string;
+	username?: string;
+	email?: string;
+	password?: string;
+	isVerified?: boolean;
+	role?: Role;
+	refresh_token?: string;
+}
+
+export class UpdateUserBioDto {
+	@ApiProperty({
+		example: "Always learning",
+		minLength: 4,
+		maxLength: 225,
+		description: "User Bio",
+		required: true,
+	})
+	@JoiSchema(Joi.string().min(4).max(225).required().messages(joiMessages))
+	bio: string;
 }
